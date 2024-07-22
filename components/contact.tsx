d84ced468687d38247c2c7e6f639efced75c19fc
Elvis Ronald Cruz Chullo <elviscruz45@Elviss-MacBook-Pro.local>
2024-07-21 19:21:43 -0500

    ver11

--- components/contact.tsx
+++ components/contact.tsx
@@ -9,7 +9,7 @@ import SubmitBtn from "./submit-btn";
 import toast from "react-hot-toast";
 
 export default function Contact() {
-  const { ref } = useSectionInView("Contact");
+  // const { ref } = useSectionInView("Contact");
   // State to manage form input values
   const [formData, setFormData] = useState({
     senderEmail: "",
@@ -31,7 +31,7 @@ export default function Contact() {
   return (
     <motion.section
       id="contact"
-      ref={ref}
+      // ref={ref}
       className="mb-20 sm:mb-28 w-[min(100%,38rem)] text-center"
       initial={{
         opacity: 0,
