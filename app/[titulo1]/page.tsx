8bb361d1d3f2c9468b627a425c114d7bb3f10646
Elvis Ronald Cruz Chullo <elviscruz45@Elviss-MacBook-Pro.local>
2024-08-12 17:07:33 -0500

    ver26

--- app/[titulo1]/page.tsx
+++ app/[titulo1]/page.tsx
@@ -7,93 +7,93 @@ import HeaderLarge from "@/components/headerLarge";
 import Image from "next/image";
 import { BlogContenido } from "@/components/blogs/BlogContenido";
 
-function getPostContent(slug: any) {
-  const folder = "recipes/";
-  const file = folder + `${slug}.md`;
-  const content = fs.readFileSync(file, "utf8");
+// function getPostContent(slug: any) {
+//   const folder = "recipes/";
+//   const file = folder + `${slug}.md`;
+//   const content = fs.readFileSync(file, "utf8");
 
-  const matterResult = matter(content);
-  return matterResult;
-}
+//   const matterResult = matter(content);
+//   return matterResult;
+// }
 
-export const generateStaticParams = async () => {
-  const posts = getPostMetadata("recipes");
-  return posts.map((post) => ({ slug: post.slug }));
-};
+// export const generateStaticParams = async () => {
+//   const posts = getPostMetadata("recipes");
+//   return posts.map((post) => ({ slug: post.slug }));
+// };
 
-export async function generateMetadata({ params, searchParams }: any) {
-  const id = params?.slug ? " ⋅ " + params?.slug : "";
-  return {
-    title: `${id.replaceAll("_", " ")}`,
-  };
-}
+// export async function generateMetadata({ params, searchParams }: any) {
+//   const id = params?.slug ? " ⋅ " + params?.slug : "";
+//   return {
+//     title: `${id.replaceAll("_", " ")}`,
+//   };
+// }
 
 export default function RecipePage(props: any) {
-  const slug = props.params.titulo1;
-  const post = getPostContent(slug);
+  // const slug = props.params.titulo1;
+  // // const post = getPostContent(slug);
 
-  const headdata = getPostMetadata("recipes");
-  // filter by the slug
-  const metadata = headdata.filter((post) => post.slug === slug)[0];
+  // const headdata = getPostMetadata("recipes");
+  // // filter by the slug
+  // const metadata = headdata.filter((post) => post.slug === slug)[0];
 
   return <BlogContenido />;
 
-  return (
-    <>
-      <div className="w-2/3 m-auto">
-        <main>
-          <HeaderLarge />
-          <div className="blog-detail-header">
-            <div className="flex flex-row justify-between mb-2">
-              <div className="flex items-center">
-                <div className="flex-shrink-0">
-                  <a href="#">
-                    {/* <span className="sr-only">Elvis</span> */}
-                    <div className="relative h-10 w-10 !mb-0">
-                      <Image
-                        priority
-                        layout="fill"
-                        objectFit="cover"
-                        className="rounded-full"
-                        src="/images/ElvisProfile.jpeg"
-                        alt=""
-                      />
-                    </div>
-                  </a>
-                </div>
-                <div className="ml-3">
-                  <p className="text-sm font-medium !mb-0">
-                    <a href="#" className="hover:underline">
-                      Elvis Cruz Chullo
-                    </a>
-                  </p>
-                  <div className="flex space-x-1 text-sm ">
-                    <time dateTime={"2022-12-12"}>{metadata.time}</time>
-                  </div>
-                </div>
-              </div>
-              <div className="flex self-end">{/* Social Links Here */}</div>
-            </div>
-            <h1 className="font-bold text-4xl mb-1 ">{metadata.title}</h1>
-            <h2 className="blog-detail-header-subtitle mb-2 text-xl ">
-              {metadata.description}
-            </h2>
-            <div className="h-96 bg-black mx-auto w-full relative">
-              <Image
-                priority
-                layout="fill"
-                objectFit="cover"
-                src={metadata.imageUrl}
-                alt=""
-              />
-            </div>
-            <br />
-          </div>
-          <article>{/* <Markdown>{post.content}</Markdown> */}</article>
-        </main>
-      </div>
-    </>
-  );
+  // return (
+  //   <>
+  //     <div className="w-2/3 m-auto">
+  //       <main>
+  //         <HeaderLarge />
+  //         <div className="blog-detail-header">
+  //           <div className="flex flex-row justify-between mb-2">
+  //             <div className="flex items-center">
+  //               <div className="flex-shrink-0">
+  //                 <a href="#">
+  //                   {/* <span className="sr-only">Elvis</span> */}
+  //                   <div className="relative h-10 w-10 !mb-0">
+  //                     <Image
+  //                       priority
+  //                       layout="fill"
+  //                       objectFit="cover"
+  //                       className="rounded-full"
+  //                       src="/images/ElvisProfile.jpeg"
+  //                       alt=""
+  //                     />
+  //                   </div>
+  //                 </a>
+  //               </div>
+  //               <div className="ml-3">
+  //                 <p className="text-sm font-medium !mb-0">
+  //                   <a href="#" className="hover:underline">
+  //                     Elvis Cruz Chullo
+  //                   </a>
+  //                 </p>
+  //                 <div className="flex space-x-1 text-sm ">
+  //                   <time dateTime={"2022-12-12"}>{metadata.time}</time>
+  //                 </div>
+  //               </div>
+  //             </div>
+  //             <div className="flex self-end">{/* Social Links Here */}</div>
+  //           </div>
+  //           <h1 className="font-bold text-4xl mb-1 ">{metadata.title}</h1>
+  //           <h2 className="blog-detail-header-subtitle mb-2 text-xl ">
+  //             {metadata.description}
+  //           </h2>
+  //           <div className="h-96 bg-black mx-auto w-full relative">
+  //             <Image
+  //               priority
+  //               layout="fill"
+  //               objectFit="cover"
+  //               src={metadata.imageUrl}
+  //               alt=""
+  //             />
+  //           </div>
+  //           <br />
+  //         </div>
+  //         <article>{/* <Markdown>{post.content}</Markdown> */}</article>
+  //       </main>
+  //     </div>
+  //   </>
+  // );
 }
 
 // import { Metadata } from "next";
