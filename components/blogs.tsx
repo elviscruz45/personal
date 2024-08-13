8a0b91bb675428c887ac3598256eb42cffd8f3d2
Elvis Ronald Cruz Chullo <elviscruz45@Elviss-MacBook-Pro.local>
2024-08-12 20:40:31 -0500

    ver40

--- components/blogs.tsx
+++ components/blogs.tsx
@@ -10,35 +10,16 @@ export default function Blogs({ blogs }: any) {
   const { ref } = useSectionInView("Blog", 0.5);
 
   return (
-    <section
-      id="blog"
-      ref={ref}
-      className="scroll-mt-28 justify-center text-center  sm:mb-0 space-x-10"
-    >
+    <section id="blog" ref={ref} className="scroll-mt-28 mb-28">
       <SectionHeading>Blogs</SectionHeading>
-      <div
-        // className="  justify-center text-center mb-28 sm:mb-40 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3
-        // "
-        className="  justify-center text-center mb-28 sm:mb-40 grid grid-cols-1 
-        "
-      >
-        {
-          <div>
-            {blogs.map((project: any, index: number) => (
-              <React.Fragment key={index}>
-                <Blog {...project} />
-              </React.Fragment>
-            ))}
-          </div>
-        }
 
-        {/* <div>
-          {blogs.map((project, index) => (
-            <React.Fragment key={index}>
-              <Blog {...project} />
-            </React.Fragment>
-          ))}
-        </div> */}
+      <div>
+        {blogs.map((project: any, index: number) => (
+          <React.Fragment key={index}>
+            <Blog {...project} />
+            <br />
+          </React.Fragment>
+        ))}
       </div>
     </section>
   );
